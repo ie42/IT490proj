282c764f1900314b16a4b98146791fe8eecf082f
Isfar Ehsan <118141419+ie42@users.noreply.github.com>
2023-11-07 20:01:41 -0500

    Update updateUserProfile.php

--- updateUserProfile.php
+++ updateUserProfile.php
@@ -4,23 +4,8 @@ require_once('dbConnect.php'); // Include your database connection code
 function updateProfileSettings($sessionID, $data) {
     $mysqli = dbConnect(); // Establish a database connection
 
-    // Ensure that the user associated with the session ID exists
-    $checkQuery = "SELECT UserID FROM Profiles WHERE SessionID = ?";
-    if ($checkStmt = $mysqli->prepare($checkQuery)) {
-        $checkStmt->bind_param("s", $sessionID);
-        $checkStmt->execute();
-        $checkStmt->store_result();
-
-        if ($checkStmt->num_rows === 0) {
-            $checkStmt->close();
-            return ["status" => 404, "message" => "User not found"]; // User not found
-        }
-
-        $checkStmt->close();
-    }
-
     // Update the user's settings
-    $updateQuery = "UPDATE Profiles SET FavoriteDirector = ?, FavoriteGenre = ?, FavoriteMovie = ?, FavoriteActor = ?, Bio = ? WHERE SessionID = ?";
+    $updateQuery = "UPDATE Users SET FavoriteDirector = ?, FavoriteGenre = ?, FavoriteMovie = ?, FavoriteActor = ?, Bio = ? WHERE SessionID = ?";
     
     if ($updateStmt = $mysqli->prepare($updateQuery)) {
         $updateStmt->bind_param("ssssss", $data['FavoriteDirector'], $data['FavoriteGenre'], $data['FavoriteMovie'], $data['FavoriteActor'], $data['Bio'], $sessionID);
@@ -36,23 +21,19 @@ function updateProfileSettings($sessionID, $data) {
     return ["status" => 500, "message" => "Failed to update settings"]; // Failed to update settings
 }
 
-// Get the session ID from cookies
-$sessionID = $_COOKIE['session_id']; // Assuming the session ID is stored in a cookie
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
 
-if ($sessionID) {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $data = json_decode(file_get_contents('php://input'), true);
-
-        if (!empty($data)) {
-            $result = updateProfileSettings($sessionID, $data);
-            http_response_code($result['status']);
-            echo json_encode(["message" => $result['message']]);
-            exit;
-        }
+    if (!empty($data) && isset($data['session_id'])) {
+        $sessionID = $data['session_id'];
+        $result = updateProfileSettings($sessionID, $data);
+        http_response_code($result['status']);
+        echo json_encode(["message" => $result['message']]);
+        exit;
     }
     echo "Invalid or missing data."; // Handle the case of missing or invalid data
 } else {
-    echo "Session ID not found in cookies."; // Handle the case where the session ID is not found
+    echo "Invalid request method."; // Handle requests other than POST
 }
 
 $mysqli->close(); // Close the database connection
