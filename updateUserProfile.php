e1be5849b83d09a5ab4645fb6ceec3fd6fcfaeda
Isfar Ehsan <118141419+ie42@users.noreply.github.com>
2023-11-10 17:32:37 -0500

    Update updateUserProfile.php

--- updateUserProfile.php
+++ updateUserProfile.php
@@ -1,6 +1,10 @@
 <?php
 require_once('dbConnect.php'); // Include your database connection code
 
+function dbConnect() {
+    return new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+}
+
 function updateProfileSettings($sessionID, $data) {
     $mysqli = dbConnect(); // Establish a database connection
 
@@ -21,7 +25,7 @@ function updateProfileSettings($sessionID, $data) {
     return ["status" => 500, "message" => "Failed to update settings"]; // Failed to update settings
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+function handlePostRequest() {
     $data = json_decode(file_get_contents('php://input'), true);
 
     if (!empty($data) && isset($data['session_id'])) {
@@ -32,9 +36,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
     echo "Invalid or missing data."; // Handle the case of missing or invalid data
-} else {
+}
+
+function handleInvalidRequestMethod() {
     echo "Invalid request method."; // Handle requests other than POST
 }
 
-$mysqli->close(); // Close the database connection
+function processRequest() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        handlePostRequest();
+    } else {
+        handleInvalidRequestMethod();
+    }
+
+    $mysqli = dbConnect();
+    $mysqli->close(); // Close the database connection
+}
+
+processRequest();
 ?>
