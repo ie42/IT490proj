e859e7e3801e3291143d4d7d21bcac598613c224
Isfar Ehsan <118141419+ie42@users.noreply.github.com>
2023-11-10 17:39:30 -0500

    Update SessionID.php

--- SessionID.php
+++ SessionID.php
@@ -1,59 +1,77 @@
 <?php
 // sessionValidate.php
 
-if (isset($_POST['sessionid'])) {
-    $sessionid = $_POST['sessionid'];
+function validateSession($sessionID) {
+    // Implement your session validation logic here.
+    // Example: Check if the session ID is valid.
+    // Replace this with your actual session validation code.
+    return true; // Replace with your validation logic
+}
 
-    // Now you can use the $sessionid variable in your PHP code
-    // for session validation or other purposes.
-    
-    // Example: Validate the session by checking if the session ID is valid.
-    if (validateSession($sessionid)) {
-        // Session is valid, return a success response.
-        $response = array('returnCode' => '1');
-        echo json_encode($response);
+function processRequest() {
+    if (isset($_POST['sessionid'])) {
+        $sessionid = $_POST['sessionid'];
+
+        // Now you can use the $sessionid variable in your PHP code
+        // for session validation or other purposes.
+        
+        // Example: Validate the session by checking if the session ID is valid.
+        if (validateSession($sessionid)) {
+            // Session is valid, return a success response.
+            $response = array('returnCode' => '1');
+            echo json_encode($response);
+        } else {
+            // Session is invalid, return an error response.
+            $response = array('returnCode' => '0');
+            echo json_encode($response);
+        }
     } else {
-        // Session is invalid, return an error response.
-        $response = array('returnCode' => '0');
+        // Handle the case when sessionid is not provided in the request.
+        // You can return an error response or take appropriate action.
+        // For example, redirect the user to the login page.
+    }
+}
+
+processRequest();
+?>
+
+
+
+
+/*
+// THIS IS FOR USING SESSION ID TO USE THE DATA FOR A USER DATA SUCH AS RECOMMENDATIONS OR PROFILE DATA
+<?php
+// sessionValidate.php
+
+function validateSession($sessionID) {
+    // Replace the following line with your actual session validation logic
+    // Example: Check if the session ID is valid, not expired, and associated with a logged-in user.
+    return true; // Replace with your validation logic
+}
+
+function processRequest() {
+    if (isset($_POST['sessionid'])) {
+        $sessionid = $_POST['sessionid'];
+
+        // Validate the session by checking if the session ID is valid.
+        if (validateSession($sessionid)) {
+            // Session is valid, return a success response.
+            $response = array('returnCode' => '1');
+            echo json_encode($response);
+        } else {
+            // Session is invalid, return an error response.
+            $response = array('returnCode' => '0', 'message' => 'Invalid session');
+            echo json_encode($response);
+        }
+    } else {
+        // Handle the case when sessionid is not provided in the request.
+        // You can return an error response or take appropriate action.
+        // For example, redirect the user to the login page.
+        $response = array('returnCode' => '0', 'message' => 'Session ID is missing');
         echo json_encode($response);
     }
-} else {
-    // Handle the case when sessionid is not provided in the request.
-    // You can return an error response or take appropriate action.
-    // For example, redirect the user to the login page.
 }
+
+processRequest();
 ?>
-/* This CODE NEEDS TO BNE ADDED TO FRONT END FOR THIS TO WORK
-function validateSession() {
-    sessionData.type = "validate_session";
-    console.log(sessionData);
-
-    $.ajax({
-        url: 'sessionValidate.php',
-        method: 'POST',
-        data: sessionData,
-        dataType: 'json',
-        success: function (result) {
-            if (result.returnCode != "1") {
-                document.cookie = "sessionid=;username=;path='/'";
-                location.href = "index.html";
-            }
-        },
-        error: function () {
-            console.log("Error validating session");
-        }
-    });
-}
-
-function logout() {
-    sessionData.type = "logout";
-
-    $.ajax({
-        url: 'sessionValidate.php',
-        method: 'POST',
-        data: sessionData,
-        dataType: 'json',
-        success: function (result) {
-            if (result.returnCode === "
 */
-// THIS IS FOR USING SESSION ID TO USE THE DATA FOR A USER DATA SUCH AS RECOMMENDATIONS OR PROFILE DATA
